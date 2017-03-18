7b7dfab2571130a16a8982ecb2f5c2791fbd6a42
Sam McCollum <smccollum@mccollum.enterprises>
2017-03-18 05:03:36 -0700

    working service, already in maven, so not to be worried over right now

--- src/main/java/enterprises/mccollum/wmapp/ssauthclient/AuthRequestFilter.java
+++ src/main/java/enterprises/mccollum/wmapp/ssauthclient/AuthRequestFilter.java
@@ -1,48 +1,132 @@
 package enterprises.mccollum.wmapp.ssauthclient;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.interfaces.RSAPublicKey;
+import java.lang.annotation.Annotation;
+import java.nio.charset.StandardCharsets;
+import java.security.Signature;
 import java.util.Base64;
 
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
 
-import enterprises.mccollum.wmapp.authobjects.AuthConstants;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
+import enterprises.mccollum.wmapp.authobjects.UserGroup;
+import enterprises.mccollum.wmapp.authobjects.UserToken;
+
+@Provider
 public class AuthRequestFilter implements ContainerRequestFilter{
 
 	@Inject
 	PublicKeySingleton pks;
 	
-	@Override
-	public void filter(ContainerRequestContext requestContext) throws IOException {
-		//TODO Auto-generated method 
-		String userTokenCt = requestContext.getHeaderString(AuthConstants.USER_TOKEN_HEADER);
+	@Context
+	ResourceInfo resourceInfo;
+	
+	private boolean validateTokenSig(ContainerRequestContext ctx, String tokenString, String sigB64){
+		if(tokenString == null || sigB64 == null){
+			return false;
+		}
+
+		byte[] tokenBytes = tokenString.getBytes(StandardCharsets.UTF_8);
+		byte[] signature = Base64.getDecoder().decode(sigB64);
 		try {
-			Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE, (RSAPublicKey) pks.getPublicKey());
-			byte[] tokenCb = Base64.getDecoder().decode(userTokenCt); //decode base64 encoded string into the appropriate bytes
-			byte[] userTokenPb = cipher.doFinal(tokenCb);
-			userTokenPt = //bytes to string
+			Signature verifier = Signature.getInstance("SHA256withRSA");
+			verifier.initVerify(pks.getPublicKey());
+			verifier.update(tokenBytes);
+			if(!verifier.verify(signature)){ //couldn't verify signature
+				return false;
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+		return true;
 	}
 	
-	
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		if(resourceInfo.getResourceClass().getAnnotation(WestmontOnly.class) == null
+				&& resourceInfo.getResourceMethod().getAnnotation(WestmontOnly.class) == null)
+			return; //if the class and method don't care about being secured, we don't either
+		
+		String tokenString = requestContext.getHeaderString(UserToken.TOKEN_HEADER);
+		
+		String signatureB64 = requestContext.getHeaderString(UserToken.SIGNATURE_HEADER);
 
+		if(!validateTokenSig(requestContext, tokenString, signatureB64)){
+			abort(requestContext);
+			return;
+		}
+		UserToken token = null;
+		try{
+			Gson gson = new Gson();
+			token = gson.fromJson(tokenString, UserToken.class);
+		}catch(JsonSyntaxException e){
+			abort(requestContext); //if we can't instantiate an object, get out
+			return;
+		}
+		
+		if(token.getExpirationDate() <= System.currentTimeMillis()){ //check expiration date
+			abort(requestContext); //it's expired already
+			return;
+		}
+		
+		if(!checkAuthorization(token)){
+			abort(requestContext);
+			return;
+		}
+	}
+
+	private boolean checkAuthorization(UserToken token) {
+		EmployeeTypesOnly eTypeAnno = null;
+		Annotation temp = null;
+		if((temp = resourceInfo.getResourceMethod().getAnnotation(EmployeeTypesOnly.class)) != null)
+			eTypeAnno = (EmployeeTypesOnly) temp;
+		else if((temp = resourceInfo.getResourceClass().getAnnotation(EmployeeTypesOnly.class)) != null)
+			eTypeAnno = (EmployeeTypesOnly) temp;
+		
+		if(eTypeAnno != null){
+			boolean pass = false;
+			for(String et : eTypeAnno.value()){
+				if(et.equals(token.getEmployeeType()))
+					pass = true;
+			}
+			if(!pass)
+				return false;
+		}
+		
+		/*NO NEED FOR THIS BLOCK RIGHT NOW
+		UserGroupsOnly ugAnno = null;
+		if((temp = resourceInfo.getResourceMethod().getAnnotation(UserGroupsOnly.class)) != null)
+			ugAnno = (UserGroupsOnly) temp;
+		else if((temp = resourceInfo.getResourceClass().getAnnotation(UserGroupsOnly.class)) != null)
+			ugAnno = (UserGroupsOnly) temp;
+		if(ugAnno != null){
+			for(String gName : ugAnno.value()){
+				if(!userInGroup(token, gName))
+					return false;
+			}
+		}//*/
+		return true;
+	}
+
+	private boolean userInGroup(UserToken token, String gName) {
+		for(UserGroup group : token.getGroups()){
+			if(group.getName().equals(gName))
+				return true;
+		}
+		return false;
+	}
+
+	private void abort(ContainerRequestContext requestContext) {
+		requestContext.abortWith(Response.status(Status.UNAUTHORIZED).build());
+	}
 }
--- src/main/java/enterprises/mccollum/wmapp/ssauthclient/PublicKeySingleton.java
+++ src/main/java/enterprises/mccollum/wmapp/ssauthclient/PublicKeySingleton.java
@@ -11,29 +11,45 @@ import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
-import enterprises.mccollum.wmapp.authobjects.AuthConstants;
 
 @Singleton
 @Startup
 public class PublicKeySingleton {
+	/**
+	 * This key will contain the public and private keys used to sign content for auth
+	 */
+	PublicKey pubKey;
 	
-	PublicKey pubKey = null;
+	public static final String KEYSTORE_PATH = "/home/smccollum/wmks.jks";
+	private static final char[] KEYSTORE_PASS = "password".toCharArray();
+	
+	public static final String KEY_ALIAS = "WMAUTH";
+	//private static final char[] KEY_PASS = "password".toCharArray();
 	
 	@PostConstruct
-	public void fetchPubKey(){
-		
+	public void init(){
 		try {
-			FileInputStream is = new FileInputStream(AuthConstants.keyStorePath);
-			KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-			keystore.load(is, null);
-			Certificate cert = keystore.getCertificate(AuthConstants.pubKeyAlias);
-			pubKey = cert.getPublicKey();	
+			pubKey = loadPubKey();
 		} catch (Exception e) {
+			System.out.print("Honestly, I'm just sick of this at this point. I've been coding for hours trying to eliminate errors and this is what happens. I don't know what went wrong. Try checking line 44 of the file CryptoSingleton.java to see what's up here.\n");
 			e.printStackTrace();
 		}
 	}
+	
 	@Lock(LockType.READ)
 	public PublicKey getPublicKey(){
 		return pubKey;
+	}
+	
+	private PublicKey loadPubKey() throws Exception{
+		KeyStore ks = readKeyStore(KEYSTORE_PATH);
+		Certificate cer = ks.getCertificate(KEY_ALIAS); //get public key, part I
+		return cer.getPublicKey();
+	}
+	
+	private KeyStore readKeyStore(String ksPath) throws Exception{
+		KeyStore ks = KeyStore.getInstance("JKS");
+		ks.load(new FileInputStream(ksPath), KEYSTORE_PASS);
+		return ks;
 	}
 }
